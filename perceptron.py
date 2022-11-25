eca0a4d0737868d453db4ec9c9e23e77739c8f10
Arthur Baudot <arthur.baudot@telecomnancy.eu>
2022-11-24 20:01:30 -0500

    Perceptron (version 1)

---
+++ perceptron.py
@@ -0,0 +1,56 @@
+"""
+Dans cette classe, on implémente la méthode du perceptron, K classes et d dimensions.
+"""
+
+from sklearn.linear_model import SGDClassifier
+import numpy as np
+import matplotlib.pyplot as plt
+
+class perceptron:
+
+    def __init__(self,lamb):
+        """
+        Forme de W : matrice dxK : W = (w1...wK), où wi = T(w1i,w2i,...,wdi).
+        """
+        self.W = None #matrice des paramètres (biais NON inclus)
+        self.W_0 = None #biais
+        self.lamb = lamb #terme de régularisation (hyper-paramètre)
+
+    def entrainement(self, x_train, t_train):
+        """
+        Paramètres à inclure dans la fonction SGDCClassifier.
+        Dans le cas d'un MAP, E(W) = fonction_perte + lamb*R(W).
+        --> Fonction_perte = celle du perceptron => loss="perceptron"
+        --> R(W) : terme de régularisation : on prend la norme 2 ici => penalty = l2
+        --> lamb : constante devant le terme de régularisation => alpha = lamb
+        --> eta0 : le learning rate dans la descente de gradient        
+        """
+        modele = SGDClassifier(loss="perceptron",penalty="l2",alpha=self.lamb,learning_rate="constant",eta0=0.001,max_iter=1000)
+        modele.fit(x_train,t_train)
+        self.W = modele.coef_ 
+        self.W_0 = modele.intercept_
+    
+    def prediction(self,x,reference):
+        """
+        Détermine y = np.transpose(W)*x où x est une entrée 1D
+        """
+        y = self.W_0 + np.dot(self.W,np.transpose(x))
+        indice = np.argmax(y)
+        return reference[indice]
+
+    def erreur(self,t,pred):
+        """
+        Retourne 1 si t!=pred, 0 sinon.
+        pred est l'espèce prédite pour une entrée x donnée, t est la cible attendue pour ce x.
+        """
+        if t==pred:
+            return 0
+        else:
+            return 1
+
+    def afficher_donnees(self,x_train):
+        plt.figure(0)
+        plt.scatter(x_train[:,0],x_train[:,1])
+        plt.show()
+
+
--- readData.py
+++ readData.py
@@ -1,17 +1,26 @@
 import pandas as pd
 
 class readData:
-    def __init__(self,train_filename, test_filename):
+    def __init__(self,train_filename, test_filename, reference_filename):
         self.train_filename = train_filename
         self.test_filename = test_filename
+        self.reference_filename = reference_filename
 
     def extract_train_data(self):
         df_train = pd.read_csv(self.train_filename)
-        x_train = df_train.iloc[:,2:]
-        t_train = df_train.iloc[:,[1]]
+        x_train = df_train.iloc[:,2:].to_numpy()
+        t_train = df_train.iloc[:,1].to_numpy()
         return x_train, t_train
 
     def extract_test_data(self):
         df_test = pd.read_csv(self.test_filename)
-        x_test = df_test.iloc[:,1:]
+        x_test = df_test.iloc[:,1:].to_numpy()
         return x_test
+
+    def create_reference(self):
+        """
+        Crée la liste des références : tableau des espèces possibles, avec leurs indices
+        """
+        df_reference = pd.read_csv(self.reference_filename)
+        reference = df_reference.columns.to_numpy()
+        return reference[1:] #on ne prend pas id
