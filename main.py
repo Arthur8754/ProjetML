3c99e3974e119146c7a9741c71ba63c527aacb5c
Arthur Baudot <arthur.baudot@telecomnancy.eu>
2022-11-28 20:45:05 -0500

    Début formalisation test, avec cross validation

--- main.py
+++ main.py
@@ -2,55 +2,58 @@ import lireDonnees
 import perceptron
 import visualiserDonnees
 import numpy as np
-
-def evaluate_perceptron(x_train,t_train,x_test,lamb,reference, recherche_hyper_parametres, loss, penalty, learning_rate, eta):
-    print("------------------------------")
-    print("Perceptron :")
-    if recherche_hyper_parametres:
-        print("Avec recherche d'hyper-paramètres (lambda)")
-    else:
-        print("Pas de recherche d'hyper-paramètres")
-    
-    perc = perceptron.perceptron(lamb=lamb)
-
-    # Entraînement du perceptron :
-    perc.entrainement(x_train,t_train, recherche_hyper_parametres, reference, loss, penalty, learning_rate, eta)
-
-    # Prédiction (données d'entraînement):
-    pred_train = np.array([perc.prediction(x,reference) for x in x_train])
-
-    # Erreur d'entraînement :
-    erreur_train_array = np.array([perc.erreur(t,pred) for t,pred in zip(t_train,pred_train)])
-    erreur_train = round(100*np.count_nonzero(erreur_train_array)/len(erreur_train_array),1)
-    print(f"Erreur d'entraînement : {erreur_train} %")
-    print("------------------------------")
-
-    # Prédiction (données de test) :
-    pred_test = np.array([perc.prediction(x,reference) for x in x_test])
-    #print(pred_test)
+import testPerceptron
+import matplotlib.pyplot as plt
 
 def main():
+    """
+    Point d'entrée du programme.
+    """
+    # 1. EXTRACTION ET VISUALISATION DES DONNÉES D'ENTRAÎNEMENT :
+
     # Extraction des données d'entraînement et de test
     rd = lireDonnees.lireDonnees("data/train.csv","data/test.csv","data/sample_submission.csv")
     x_train,t_train = rd.extract_train_data()
     x_test = rd.extract_test_data()
     reference = rd.create_reference() #liste des différentes espèces possibles
-
-    # Normalisation :
     x_train_normalized,x_test_normalized = rd.normalize_data(1*x_train, 1*x_test)
 
     # Visualisation des données d'entraînement non normalisées
     vd = visualiserDonnees.visualiserDonnees(x_train,x_test,t_train,reference)
     vd.visualiserEntrainement(False)
-    #vd.visualiserTest()
 
     # Visualisation des données d'entraînement normalisées :
     vd2 = visualiserDonnees.visualiserDonnees(x_train_normalized,x_test_normalized,t_train,reference)
     vd2.visualiserEntrainement(True)
 
-    # Modèle du perceptron :
-    #loss="perceptron",penalty="l2",alpha=self.lamb,learning_rate="constant",eta0=0.001,max_iter=1000
-    evaluate_perceptron(x_train,t_train,x_test,0.01,reference, True, "perceptron", "l2", "constant",0.001)
+    # 2. TESTS SUR LE MODÈLE DU PERCEPTRON :
+    print("-------------------------------")
+    print("TESTS MODÈLES DU PERCEPTRON : ")
+
+    testPerc = testPerceptron.testPerceptron()
+
+    # Test 1 : données non normalisées, sans recherche d'hyper-paramètres :
+    err_train1, err_valid1 = testPerc.test(x_train, t_train, reference, lamb=0.01, recherche_hyper_parametres=False, loss="perceptron", penalty="l2", learning_rate="constant", k=10, eta=0.001)
+    print("# Tests sur des données non normalisées, sans recherche d'hyper-paramètres : ")
+    print(f"Erreur d'entraînement : {err_train1} %")
+    print(f"Erreur de validation : {err_valid1} %")
+    print("")
+
+    # Test 2 : données normalisées, sans recherche d'hyper-paramètres :
+    err_train2, err_valid2 = testPerc.test(x_train_normalized, t_train, reference, lamb=0.01, recherche_hyper_parametres=False, loss="perceptron", penalty="l2", learning_rate="constant", k=10, eta=0.001)
+    print("# Tests sur des données normalisées, sans recherche d'hyper-paramètres : ")
+    print(f"Erreur d'entraînement : {err_train2} %")
+    print(f"Erreur de validation : {err_valid2} %")
+    print("-------------------------------")
+
+    # Comparaison données non normalisées et normalisées :
+    plt.figure(0)
+    plt.bar(["Non normalisé, entraînement","Non normalisé, validation","Normalisé, entraînement","Normalisé, validation"],[err_train1, err_valid1, err_train2, err_valid2])
+    plt.title("Comparaison erreurs d'entraînement et de validation entre données non normalisées et normalisées")
+    plt.xlabel("Non normalisé / normalisé")
+    plt.ylabel("Erreur (%)")
+    plt.ylim(0,100)
+    plt.show()
 
 if __name__=="__main__":
     main()
--- perceptron.py
+++ perceptron.py
@@ -150,3 +150,81 @@ class perceptron:
 
         # On retient le self.lamb pour lequel l'erreur de validation moyenne est la plus faible    
         self.lamb = arg_min_lamb
+    
+    def erreur_train_and_valid(self, x_tab, t_tab, reference, k, recherche_hyper_parametres, loss, penalty, learning_rate, eta):
+        """
+        Détermine l'erreur d'entraînement et de validation. Ce calcul se fait à l'aide d'une cross validation : de façon semblable à la recherche d'hyper-paramètres, 
+        on découpe x_tab en k groupes distincts, puis, on entraîne le modèle sur k-1 groupes et on teste sur un des groupes, et on fait ça k fois, en prenant à chaque
+        fois un ensemble de validation différent.
+        """
+        # Mélange des données d'entraînement :
+        shuffler = np.random.permutation(len(x_tab)) #permute les index aléatoirement
+        x_tab_shuffled = x_tab[shuffler] #on associe x_tab aux nouveaux index
+        t_tab_shuffled = t_tab[shuffler] #on associe t_tab aux nouveaux index
+
+        # Découpage des données d'entraînement en k groupes distincts
+        x_split = np.split(x_tab_shuffled,k)
+        t_split = np.split(t_tab_shuffled,k)
+
+        # Extraction des données d'entraînement et de validation :
+        X_trains = [] #tableau à k=10 éléments : stocke les X_train pour les 10 itérations de la k fold cross validation
+        t_trains = []
+        X_valids = []
+        t_valids = []
+
+        for j in range(k):
+            # Recherche de l'ensemble de validation (un groupe sur les k groupes (à chaque fois différent))
+            X_valid = x_split[j]
+            t_valid = t_split[j]
+            
+            # Recherche de l'ensemble d'apprentissage (tout sauf le groupe de validation)
+            x_split1 = x_split[:j]
+            x_split2 = x_split[j+1:]
+            t_split1 = t_split[:j]
+            t_split2 = t_split[j+1:]
+
+            if len(x_split1) != 0 and len(x_split2)!=0:
+                X_train = np.array(np.concatenate((x_split1,x_split2)))
+                X_train = X_train.reshape((-1,len(x_tab[0])))
+                t_train = np.array(np.concatenate((t_split1,t_split2)))
+                t_train = t_train.reshape((1,-1))[0]
+            elif len(x_split1) != 0 and len(x_split2) == 0:
+                X_train = np.concatenate(x_split1)
+                t_train = np.concatenate(t_split1)
+            elif len(x_split1) == 0 and len(x_split2) !=0:
+                X_train = np.concatenate(x_split2)
+                t_train = np.concatenate(t_split2)
+            else:
+                X_train = []
+                t_train = []
+
+            X_trains.append(1*X_train)
+            t_trains.append(1*t_train)
+            X_valids.append(1*X_valid)
+            t_valids.append(1*t_valid)
+        
+        erreurs_train = np.empty(k) #tableau stockant l'erreur d'entraînement pour chaque k
+        erreurs_valid = np.empty(k) #tableau stockant l'erreur de validation pour chaque k    
+
+        # Pour chaque groupe de données :
+        for j in range(k):
+            x_apprentissage, x_validation = X_trains[j], X_valids[j]
+            t_apprentissage, t_validation = t_trains[j], t_valids[j]
+
+            # Entraînement sur x_apprentissage et t_apprentissage :
+            self.entrainement(x_apprentissage, t_apprentissage, recherche_hyper_parametres, reference, loss, penalty, learning_rate, eta)
+
+            # Prédiction et erreur pour l'entraînement :
+            pred_app = np.array([self.prediction(x,reference) for x in x_apprentissage]) #tableau des prédictions faites
+            erreur_app_array = np.array([self.erreur(t,pred) for t,pred in zip(t_apprentissage,pred_app)]) #tableau des erreurs
+            erreurs_train[j] = round(100*np.count_nonzero(erreur_app_array)/len(erreur_app_array),1)
+
+            # Prédiction et erreur pour la validation :
+            pred_valid = np.array([self.prediction(x,reference) for x in x_validation]) #tableau des prédictions faites
+            erreur_valid_array = np.array([self.erreur(t,pred) for t,pred in zip(t_validation,pred_valid)]) #tableau des erreurs
+            erreurs_valid[j] = round(100*np.count_nonzero(erreur_valid_array)/len(erreur_valid_array),1)
+        
+        erreur_train = round(np.mean(erreurs_train),1)
+        erreur_valid = round(np.mean(erreurs_valid),1)
+
+        return erreur_train, erreur_valid
