314a63542d3326722d3d2c5a3e524655505abdd0
Raphael Lion <raphael.lion@telecomnancy.eu>
2022-12-11 18:50:27 -0500

    ajout dernier classifieur et plot

--- classifieurs/modele_probabiliste.py
+++ classifieurs/modele_probabiliste.py
@@ -2,23 +2,31 @@ from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import precision_score, accuracy_score, recall_score, f1_score
 from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
 import matplotlib.pyplot as plt
 import numpy as np
 
+# define the GlobalModelClassifier class
 class GlobalModelClassifier:
+    # initialize the class with a scikit-learn classifier and a dictionary of hyperparameters to search over
     def __init__(self, classifier, parameters):
         self.classifier = classifier
         self.parameters = parameters
-    
+
+    # define a method to train the classifier on training data
     def train(self, X_train, y_train):
         self.classifier.fit(X_train, y_train)
-    
+
+    # define a method to make predictions on new data
     def predict(self, X_test):
         return self.classifier.predict(X_test)
     
+    # define a method to set the hyperparameters of the classifier
     def setParameter(self, parameter):
         self.classifier.set_params(**parameter)
 
+    # define a method to search for the best hyperparameters for the classifier
     def hyperparameter_search(self, X, y, k, graphic=False):
 
         clf = GridSearchCV(estimator=self.classifier,param_grid=self.parameters,cv=k,verbose=1, scoring='accuracy')
@@ -27,8 +35,6 @@ class GlobalModelClassifier:
         if graphic:
             
             cvres = clf.cv_results_
-            cvres
-            len(cvres["params"])
             list_param = []
 
             for i in range(0,len(cvres["params"])):
@@ -47,17 +53,17 @@ class GlobalModelClassifier:
             plt.show()
 
         return clf.best_params_
-    
-    def error(self, y_pred, y, average="micro"):        
-        precision = precision_score(y, y_pred,average=average)
-        accuracy = accuracy_score(y, y_pred)
-        recall = recall_score(y, y_pred,average=average)
-        f1 = f1_score(y, y_pred,average=average)
-        
-        return {'precision': precision, 'accuracy': accuracy, 'recall': recall, 'f1': f1}
 
+    # define a method to calculate different metrics of the classifier on data
+    def error(self, y_pred, y, average="micro"):        
+        f1 = f1_score(y, y_pred,average=average)
+        return {'f1-score': f1}
+
+# define the GaussianNB_Classifier class that inherits from the GlobalModelClassifier class
 class GaussianNB_Classifier(GlobalModelClassifier):
+    # initialize the class with a dictionary of hyperparameters to search over
     def __init__(self, parameters=1e-9):
+        # call the __init__ method of the parent GlobalModelClassifier class, passing the GaussianNB classifier and the hyperparameters
         super().__init__(GaussianNB(), parameters)
 
 class MultinomialNB_Classifier(GlobalModelClassifier):
@@ -80,56 +86,10 @@ class Perceptron_Classifier(GlobalModelClassifier):
     def __init__(self, parameters={'alpha':0.0001}):
         super().__init__(SGDClassifier(loss='perceptron'),parameters)
 
+class RandomForest_Classifier(GlobalModelClassifier):
+    def __init__(self,parameters={'n_estimators':100}):
+        super().__init__(RandomForestClassifier(),parameters)
 
-
-#from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB
-#from sklearn.model_selection import GridSearchCV
-#from sklearn.metrics import mean_squared_error
-#
-## define the SklearnClassifier class
-#class SklearnClassifier:
-#    # initialize the class with a scikit-learn classifier and a dictionary of hyperparameters to search over
-#    def __init__(self, classifier, parameters):
-#        self.classifier = classifier
-#        self.parameters = parameters
-#    
-#    # define a method to train the classifier on training data
-#    def train(self, X_train, y_train):
-#        self.classifier.fit(X_train, y_train)
-#    
-#    # define a method to make predictions on new data
-#    def predict(self, X_test):
-#        return self.classifier.predict(X_test)
-#    
-#    # define a method to search for the best hyperparameters for the classifier
-#    def hyperparameter_search(self, X, y):
-#        clf = GridSearchCV(self.classifier, self.parameters)
-#        clf.fit(X, y)
-#        return clf.best_params_
-#    
-#    # define a method to calculate the mean squared error of the classifier on data
-#    def error(self, X, y):
-#        y_pred = self.predict(X)
-#        return mean_squared_error(y, y_pred)
-#
-## define the GaussianNBClassifier class that inherits from the SklearnClassifier class
-#class GaussianNBClassifier(SklearnClassifier):
-#    # initialize the class with a dictionary of hyperparameters to search over
-#    def __init__(self, parameters):
-#        # call the __init__ method of the parent SklearnClassifier class, passing the GaussianNB classifier and the hyperparameters
-#        super().__init__(GaussianNB(), parameters)
-#
-## define the MultinomialNBClassifier class that inherits from the SklearnClassifier class
-#class MultinomialNBClassifier(SklearnClassifier):
-#    # initialize the class with a dictionary of hyperparameters to search over
-#    def __init__(self, parameters):
-#        # call the __init__ method of the parent SklearnClassifier class, passing the MultinomialNB classifier and the hyperparameters
-#        super().__init__(MultinomialNB(), parameters)
-#
-## define the BernoulliNBClassifier class that inherits from the SklearnClassifier class
-#class BernoulliNBClassifier(SklearnClassifier):
-#    # initialize the class with a dictionary of hyperparameters to search over
-#    def __init__(self, parameters):
-#        # call the __init__ method of the parent SklearnClassifier class, passing the BernoulliNB classifier and the hyperparameters
-#        super().__init__(BernoulliNB(), parameters)
-#
+class KNN_Classifier(GlobalModelClassifier):
+    def __init__(self,parameters={'n_neighbors':5}):
+        super().__init__(KNeighborsClassifier(),parameters)
